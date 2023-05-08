b7dbef68c2ab11eab6034bca7c12c1dc0de72de9
Jakob Engler <jengler2291@gmail.com>
2023-05-08 13:40:08 -0500

    added MUI to header jsx

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,10 +1,13 @@
-
+import { AppBar } from "@mui/material";
 
 function Header () {
 
     return(
         <>
+        <AppBar position="static" className="app-header"
+        sx={{ backgroundColor: 'rgba(133,15,16,1)'}}>
          <h1>The Movies Saga!</h1>
+         </AppBar>
         </>
     )
 }
