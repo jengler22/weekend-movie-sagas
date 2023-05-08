72e4882acb7865d5140dab85def02387ed3b1326
Jakob Engler <jengler2291@gmail.com>
2023-05-08 10:28:29 -0500

    added GET request in MovieDetails jsx

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,17 +1,35 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { useHistory, useParams} from "react-router-dom";
+import { useEffect } from "react";
+import axios from "axios";
 // add css later
 
 function MovieDetails () {
 
     let {id} = useParams();
     let history = useHistory();
+    let dispatch = useDispatch();
     const movies = useSelector(store => store.movies);
-    const genres = useSelector(store => store.genres);
+    const movieGenres = useSelector(store => store.movieGenres);
 
     const backButton = () => {
         history.push('/')
     }
+
+    // GET genres of movie by id
+    const getMovieGenres = () => {
+        console.log('In getMovieGenres, movieGenres');
+        axios.get(`/api/movie/${movies[id - 1].id}`)
+        .then((response) => {
+            dispatch({ type: 'SET_MOVIE_GENRES', payload: response.data });
+        }).catch(error => {
+            alert('Something went wrong');
+        })
+    }
+
+    useEffect(() => {
+        getMovieGenres();
+    }, [])
 
     return(
         <>
@@ -19,6 +37,17 @@ function MovieDetails () {
         <img src={movies[id-1].poster} alt={movies[id-1].title} />
         <br />
         <p className="detailsDescription"> {movies[id-1].description}</p>
+        <h4>Genres</h4>
+        <ul className="genreList">
+            {
+                movieGenres.map(genre => {
+                    return <li key= {genre.id}>
+                        {genre.name}
+                    </li>
+                    
+                })
+            }
+        </ul>
         <br />
         <button onClick={backButton}> Movie List </button>
         </>
