8998b74e44df317d0f4e35ea000b5ca73be81d7c
Jakob Engler <jengler2291@gmail.com>
2023-05-08 13:31:14 -0500

    added MUI to movieList jsx

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,6 +2,13 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import './MovieList.css'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Container from '@mui/material/Container';
+
 
 function MovieList() {
     const history = useHistory();
@@ -22,22 +29,55 @@ function MovieList() {
      }
 
     return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}
-                                  onClick={ () => moreDetails(movie.id)}
-                                  className="movieImages"/>
+        // <main>
+        //     <h1>MovieList</h1>
+        //     <section className="movies">
+        //         {movies.map(movie => {
+        //             return (
+        //                 <div key={movie.id} >
+        //                     <h3>{movie.title}</h3>
+        //                     <img src={movie.poster} alt={movie.title}
+        //                           onClick={ () => moreDetails(movie.id)}
+        //                           className="movieImages"/>
                             
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
+        //                 </div>
+        //             );
+        //         })}
+        //     </section>
+        // </main>
+        <>
+        <Typography variant="h4">NOW SHOWING</Typography>
+            <p><i>- Click for more details! -</i></p>
+            <Container maxWidth='xl'>
+                <Grid container spacing={5} justifyContent='center'>
+                        {movies.map(movie => {
+                            return (
+                                // spacing between cards
+                                <Grid item sx={{ mx: '20px', my: '20px' }}>
+
+                                    {/* Card for each movie */}
+                                    <Card key={movie.id} variant="outlined"
+                                        sx={{ width: 320, maxWidth: 320, 
+                                            height: 432, maxHeight: 432,
+                                            backgroundColor: 'rgba(133,15,16,1)',
+                                            border: '10px solid black' }}
+                                    >
+                                        <Typography variant="h6" sx={{ my: '10px' }}>
+                                            {movie.title}                         
+                                        </Typography>
+                                        <img src={movie.poster} alt={movie.title}
+                                            onClick={() => moreDetails(movie.id)}
+                                            className="movieImages"
+                                        />
+                                    </Card>
+                                </Grid>
+                            );
+                        })}
+                </Grid>
+            </Container>
+            <br />
+        
+        </>
 
     );
 }
